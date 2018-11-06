7639152f620b508a94d3272a4cbd44673e8962b6
吴佳杰 <wujiajie@zhuojianchina.com>
2018-11-06 15:01:11 +0800

    init

---
+++ clouddisk-service/src/main/java/com/dinry/clouddisk/config/TkMapperConfig.java
@@ -0,0 +1,31 @@
+package com.dinry.clouddisk.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import tk.mybatis.spring.mapper.MapperScannerConfigurer;
+
+import java.util.Properties;
+
+/**
+ * @Author: 吴佳杰
+ * @Date: 2018/11/6 14:55
+ * @Description:
+ */
+@Configuration
+public class TkMapperConfig {
+
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer(){
+        MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
+        mapperScannerConfigurer.setBasePackage("com.dinry.clouddisk.dao");
+        Properties propertiesMapper = new Properties();
+        //通用mapper位置，不要和其他mapper、dao放在同一个目录
+        propertiesMapper.setProperty("mappers", "com.dinry.clouddisk.config.MyMapper");
+        propertiesMapper.setProperty("notEmpty", "false");
+        //主键UUID回写方法执行顺序,默认AFTER,可选值为(BEFORE|AFTER)
+        propertiesMapper.setProperty("ORDER","BEFORE");
+        mapperScannerConfigurer.setProperties(propertiesMapper);
+        return mapperScannerConfigurer;
+    }
+
+}
